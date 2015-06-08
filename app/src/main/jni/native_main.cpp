f1b5b26aede7f0ce81b94a101fac1d520ed1b1d3
AlexTape <alexalone@web.de>
2015-06-08 15:25:30 +0200

    updated to opencv 2.4.11

--- app/src/main/jni/native_main.cpp
+++ app/src/main/jni/native_main.cpp
@@ -57,7 +57,8 @@ void native_gl_resize(JNIEnv *env UNUSED, jclass clazz UNUSED, jint width, jint 
 
 void native_gl_render(JNIEnv *env UNUSED, jclass clazz UNUSED)
 {
-	//LOGI("native_render ");
+
+	LOGI("native_gl_render");
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	// Clear Screen And Depth Buffer
 	glLoadIdentity();									// Reset The Current Modelview Matrix
 	glScalef(2,2,2);
@@ -76,7 +77,7 @@ void native_gl_render(JNIEnv *env UNUSED, jclass clazz UNUSED)
 
  	GLfloat vertex[3*2*10];
  	int vertexIndex = 0; 
- 	vertex[vertexIndex++] =-20.0;
+ 	vertex[vertexIndex++] =-30.0;
  	vertex[vertexIndex++] =-10.0;
  	vertex[vertexIndex++] = 5; 
  	vertex[vertexIndex++] =-20.0;
