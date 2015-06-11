92621490dd53418ac051b7b5fc23f11078a60ba3
Simon Burfield <simon.burfield@o2.com>
2015-06-11 20:36:08 +0100

    Initial commit.

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -6,6 +6,12 @@ LOCAL_MODULE    := opencv_nonfree
 LOCAL_SRC_FILES := lib/libnonfree.so
 include $(PREBUILT_SHARED_LIBRARY)
 
+include $(CLEAR_VARS) 
+
+LOCAL_MODULE := lgnustl_shared 
+LOCAL_SRC_FILES := lib/libgnustl_shared.so
+include $(PREBUILT_SHARED_LIBRARY)
+
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := opencv_java
@@ -31,7 +37,7 @@ include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 
-LOCAL_C_INCLUDES:= /home/thinker/Android/OpenCV-2-4-10/sdk/native/jni/include
+LOCAL_C_INCLUDES:= /Users/burfies1/Downloads/OpenCV-android-sdk/sdk/native/jni/include/
 LOCAL_MODULE    := native_openmaka
 
 FILE_LIST := $(wildcard $(LOCAL_PATH)/**/*.cpp)
@@ -40,11 +46,11 @@ FILE_LIST += $(wildcard $(LOCAL_PATH)/*.cpp)
 LOCAL_SRC_FILES := $(FILE_LIST:$(LOCAL_PATH)/%=%)
 
 LOCAL_STATIC_LIBRARIES := libpng
-LOCAL_SHARED_LIBRARIES := opencv_nonfree opencv_java
+LOCAL_SHARED_LIBRARIES := opencv_nonfree opencv_java lgnustl_shared 
 
 LOCAL_CFLAGS    := -Werror -O3 -ffast-math -DNDEBUG -DANDROID_NDK -DDISABLE_IMPORTGL -DOPEL_ES_1 -ffast-math
 LOCAL_CXXFLAGS  += -fno-exceptions
-LOCAL_LDLIBS    += -L$(LOCAL_PATH)/lib -landroid -llog -ldl -lEGL -lGLESv1_CM -lOpenSLES -lGLESv2
+LOCAL_LDLIBS    += -L$(LOCAL_PATH)/lib -landroid -llog -ldl -lEGL -lGLESv1_CM -lOpenSLES -lGLESv2 -lgnustl_shared
 LOCAL_DISABLE_FORMAT_STRING_CHECKS := true
 
 
