d6db0302da9d6c6cf327b1d896349c43489b5e4b
AlexTape <alexalone@web.de>
2015-08-07 15:11:50 +0200

    fixed shutdown on rotation change.

--- app/src/main/jni/native_main.cpp
+++ app/src/main/jni/native_main.cpp
@@ -1,16 +1,17 @@
 #include <jni.h>
 #include <string.h>
 #include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
 #include "native_main.h"
 #include "native_logger.h"
 
-#include "Application/Controller.hpp"
-
 JNIEXPORT void JNICALL Java_de_alextape_openmaka_NativeController_native_1start
-  (JNIEnv *, jclass clazz)
+  (JNIEnv *env, jclass clazz)
 {
-    Controller::getInstance()->start();
+//    Controller::getInstance()->start();
 }
 
 JNIEXPORT jint JNICALL Java_de_alextape_openmaka_NativeController_native_1initialize
@@ -29,7 +30,7 @@ JNIEXPORT jint JNICALL Java_de_alextape_openmaka_NativeController_native_1initia
 
     if (strMsgPtr != NULL) {
         std::string pathString(strMsgPtr);
-        i_ = Controller::getInstance()->initialize(mGray, pathString);
+//        i_ = Controller::getInstance()->initialize(mGray, pathString);
     }
 
     env->ReleaseStringChars(configPath, (jchar *)strMsgPtr);
@@ -51,7 +52,7 @@ JNIEXPORT jint JNICALL Java_de_alextape_openmaka_NativeController_native_1displa
 
     cvtColor(mRgba, mGray, CV_GRAY2RGBA);
 
-    i_ = Controller::getInstance()->displayFunction(mRgba, mGray);
+//    i_ = Controller::getInstance()->displayFunction(mRgba, mGray);
 
     env->ReleaseByteArrayElements(data, pData, 0);
     env->ReleaseIntArrayElements(pixels, pPixels, 0);
@@ -62,41 +63,41 @@ JNIEXPORT jint JNICALL Java_de_alextape_openmaka_NativeController_native_1displa
 JNIEXPORT void JNICALL Java_de_alextape_openmaka_NativeController_native_1glRender
   (JNIEnv *env, jclass clazz)
 {
-    Controller::getInstance()->glRender();
+//    Controller::getInstance()->glRender();
 }
 
 JNIEXPORT void JNICALL Java_de_alextape_openmaka_NativeController_native_1glResize
   (JNIEnv *env, jclass clazz, jint width, jint height)
 {
-    Controller::getInstance()->glResize(width, height);
+//    Controller::getInstance()->glResize(width, height);
 }
 
 JNIEXPORT void JNICALL Java_de_alextape_openmaka_NativeController_native_1touchEvent
-  (JNIEnv *, jclass, jfloat x, jfloat y, jint action)
+  (JNIEnv *env, jclass, jfloat x, jfloat y, jint action)
 {
-    Controller::getInstance()->touchEvent(x, y, action);
+//    Controller::getInstance()->touchEvent(x, y, action);
 }
 
 JNIEXPORT void JNICALL Java_de_alextape_openmaka_NativeController_native_1keyEvent
-  (JNIEnv *, jclass, jint keyCode, jint event)
+  (JNIEnv *env, jclass, jint keyCode, jint event)
 {
-    Controller::getInstance()->keyEvent(keyCode, event);
+//    Controller::getInstance()->keyEvent(keyCode, event);
 }
 
 JNIEXPORT void JNICALL Java_de_alextape_openmaka_NativeController_native_1setObjectDetection
   (JNIEnv *env, jclass clazz, jboolean isActive)
 {
-    Controller::getInstance()->setObjectDetection(isActive);
+//    Controller::getInstance()->setObjectDetection(isActive);
 }
 
 JNIEXPORT void JNICALL Java_de_alextape_openmaka_NativeController_native_1setTracking
   (JNIEnv *env, jclass clazz, jboolean isActive)
 {
-    Controller::getInstance()->setTracking(isActive);
+//    Controller::getInstance()->setTracking(isActive);
 }
 
 JNIEXPORT void JNICALL Java_de_alextape_openmaka_NativeController_native_1setOpenGL
   (JNIEnv *env, jclass clazz, jboolean isActive)
 {
-    Controller::getInstance()->setOpenGL(isActive);
+//    Controller::getInstance()->setOpenGL(isActive);
 }
