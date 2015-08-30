1defcc73d7a203f85a6ee573d6eecb244759069e
AlexTape <alexalone@web.de>
2015-08-30 16:06:51 +0200

    Fixed config.xml storage path issue to safe result frames properly

--- app/src/main/java/de/alextape/openmaka/FileManager.java
+++ app/src/main/java/de/alextape/openmaka/FileManager.java
@@ -4,12 +4,25 @@ import android.content.Context;
 import android.util.Log;
 
 import org.apache.commons.io.IOUtils;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.StringWriter;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 public class FileManager {
 
@@ -78,6 +91,78 @@ public class FileManager {
             Log.i(TAG, "BUG:" + e.getMessage());
             e.printStackTrace();
         }
+
+        // paste storage path to config.xml
+        try {
+            adapatStoragePath();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (TransformerException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void adapatStoragePath() throws ParserConfigurationException, IOException, SAXException, TransformerException {
+
+        File inputFile = new File(CONFIG_FILE_PATH);
+
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        Document document = db.parse(inputFile);
+
+        // fill path
+        document.getElementsByTagName("storagePath").item(0).getFirstChild().setTextContent(String.valueOf(STORAGE_PATH));
+
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource source = new DOMSource(document);
+
+        StreamResult result = new StreamResult(new StringWriter());
+
+        //t.setParameter(OutputKeys.INDENT, "yes");
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "5");
+        transformer.transform(source, result);
+
+        //writing to file
+        FileOutputStream fop = null;
+        File outputFile;
+        try {
+
+            outputFile = new File(CONFIG_FILE_PATH);
+            fop = new FileOutputStream(outputFile);
+
+            // if file doesnt exists, then create it
+            if (!outputFile.exists()) {
+                outputFile.createNewFile();
+            }
+
+            // get the content in bytes
+            String xmlString = result.getWriter().toString();
+            System.out.println(xmlString);
+            byte[] contentInBytes = xmlString.getBytes();
+
+            fop.write(contentInBytes);
+            fop.flush();
+            fop.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (fop != null) {
+                    fop.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 
     private void copyFile(InputStream in, OutputStream out) throws IOException {
